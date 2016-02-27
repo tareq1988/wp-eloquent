4b33623aaaf118d15eba911f6d88fcf3dd345266
Sohel Amin <sohelamincse@gmail.com>
2016-02-27 23:33:28 +0600

    moved class decl..

--- src/Eloquent/Migration.php
+++ src/Eloquent/Migration.php
@@ -2,9 +2,10 @@
 
 namespace WeDevs\ORM\Eloquent;
 
-/**
-*
-*/
+use WeDevs\ORM\Eloquent\Facades\Schema;
+use WeDevs\ORM\Eloquent\Migration;
+use WeDevs\ORM\Eloquent\Schema\Blueprint;
+
 class Migration
 {
 
